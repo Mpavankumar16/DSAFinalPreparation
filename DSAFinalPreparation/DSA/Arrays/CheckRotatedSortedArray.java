ff6956f3eab0cd178c83c320d1036bebed812a9e
Mpavankumar16 <mungandapavankumar2016@gmail.com>
2023-06-02 19:24:40 +0530

    LinkedList commit

--- DSAFinalPreparation/DSA/Arrays/CheckRotatedSortedArray.java
+++ DSAFinalPreparation/DSA/Arrays/CheckRotatedSortedArray.java
@@ -9,9 +9,13 @@ public class CheckRotatedSortedArray {
     public static boolean checkRotatedSorted(int[] arr){
         int count = 0;
         for(int i=1; i<arr.length; i++){
+            //Expecting it to be rotated atleast once 
+            //Checking if previous elem is > Current Element
             if(arr[i-1]>arr[i])
                 count++;
         }
+        //If it is not rotated then it should be sorted where
+        //Last element is > 1st element
         if(arr[arr.length-1]>arr[0])
             count++;
 
