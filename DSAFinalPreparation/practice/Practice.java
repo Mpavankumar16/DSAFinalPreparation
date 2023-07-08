a7319dacf6343f6756e7170e7ffaf21aa5fd2beb
Mpavankumar16 <mungandapavankumar2016@gmail.com>
2023-07-08 18:04:19 +0530

    Sliding Window

--- DSAFinalPreparation/practice/Practice.java
+++ DSAFinalPreparation/practice/Practice.java
@@ -8,4 +8,21 @@ public class Practice{
         HashMap<Integer, Integer> temp  = new HashMap<>();
 
     }
+
+    public int numberOfSubarrays(int[] A, int k) {
+        return atMost(A, k) - atMost(A, k - 1);
+    }
+
+    public int atMost(int[] A, int k) {
+        int res = 0;
+        int i = 0;
+        int n = A.length;
+        for (int j = 0; j < n; j++) {
+            k -= A[j] % 2;
+            while (k < 0)
+                k += A[i++] % 2;
+            res += j - i + 1;
+        }
+        return res;
+    }
 }
